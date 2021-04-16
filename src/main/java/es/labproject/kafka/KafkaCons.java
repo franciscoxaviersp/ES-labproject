b554900c16070ca09e4bf253c81b98f476289eef
franciscoxaviersp <franciscoxaviersp@ua.pt>
2021-04-16 18:05:14 +0100

    Kafka topics working

--- src/main/java/es/labproject/kafka/KafkaCons.java
+++ src/main/java/es/labproject/kafka/KafkaCons.java
@@ -1,6 +1,9 @@
 
 package es.labproject.kafka;
 
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
