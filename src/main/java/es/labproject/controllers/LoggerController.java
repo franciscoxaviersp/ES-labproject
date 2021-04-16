29b10ca85bcba4c1aa263d2010f150d02a42e67a
franciscoxaviersp <45402958+franciscoxaviersp@users.noreply.github.com>
2021-04-16 17:28:24 +0100

    App properties and docker fix

---
+++ src/main/java/es/labproject/controllers/LoggerController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.labproject.controllers;
+
+import es.labproject.kafka.KafkaCons;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Xico
+ */
+@RestController
+@RequestMapping("/logging")
+public class LoggerController {
+    private Logger logger = LoggerFactory.getLogger(LoggerController.class);
+
+    @Autowired
+    private KafkaCons consumer;
+
+    @GetMapping("/logs")
+    public List logging() {
+        logger.info("Consuming logging messages");
+
+        return consumer.getLogs();
+    }
+
+    @GetMapping("/data")
+    public List data() {
+        logger.info("Consuming data messages");
+
+        return consumer.getData();
+    }
+
+}
