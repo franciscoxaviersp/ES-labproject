00d2dd6e0ba7583b3d82c7c2f09f150af9b4f59e
franciscoxaviersp <franciscoxaviersp@ua.pt>
2021-04-16 18:02:38 +0100

    Kafka topics working

--- src/main/java/es/labproject/kafka/KafkaProd.java
+++ src/main/java/es/labproject/kafka/KafkaProd.java
@@ -5,9 +5,9 @@
  */
 package es.labproject.kafka;
 
-import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -26,12 +26,4 @@ public class KafkaProd {
         logger.info(String.format("#### -> Producing message -> %s", message));    
         this.kafkaTemplate.send(topic,message);
     }
-
-//    @Bean
-//    public NewTopic createTopic(){
-//
-//        return new NewTopic(TOPIC,1,(short) 1);
-//    }
-
-
 }
