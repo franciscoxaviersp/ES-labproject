9b49ba2eaa616dc3f13ba35a5485b29c2e213f7a
franciscoxaviersp <45402958+franciscoxaviersp@users.noreply.github.com>
2021-04-22 21:45:16 +0100

    Candle repository update

--- labproject/src/main/java/es/labproject/repository/CandleRepository.java
+++ labproject/src/main/java/es/labproject/repository/CandleRepository.java
@@ -16,4 +16,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface CandleRepository extends JpaRepository<Candle,Long>{
     List<Candle> findByopenTime(Date openTime);
+    List<Candle> findTop100ByOrderByOpenTime();
 }
